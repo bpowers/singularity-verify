0fc3e81d834621e5813fcdc5aeb52b7af76c019d
Bobby Powers <bobbypowers@gmail.com>
2019-03-31 15:36:43 -0700

    check more

--- build.mk
+++ build.mk
@@ -3,3 +3,19 @@ BUILD = $(ROOT)/../base/build
 OBJROOT = $(ROOT)/obj
 BINROOT = $(ROOT)/bin
 SPEC = $(ROOT)/src/Trusted/Spec
+
+BEAT_EXE      = $(BINROOT)/beat.exe
+BOOGIEASM_EXE = $(BINROOT)/boogieasm.exe
+BEAT          = mono $(BEAT_EXE)
+BOOGIEASM     = mono $(BOOGIEASM_EXE)
+BOOGIE        = boogie
+MKDIR         = mkdir -p
+
+all: project
+
+nucleus:
+	make -C src/Checked/Nucleus
+
+project: nucleus
+
+.PHONY: all project nucleus
